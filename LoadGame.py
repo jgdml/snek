a97ecbd66d0c9359600db974b4435e2599df13a4
jgdml <47838646+jgdml@users.noreply.github.com>
2019-09-27 21:33:41 -0300

    Update LoadGame.py

--- LoadGame.py
+++ LoadGame.py
@@ -1,4 +1,6 @@
 import pygame as engine
+from os import _exit
+from random import randint
 
 def autoRun():
     ## a direçao eh global pra agnt poder mudar ela fora da funçao
@@ -37,16 +39,51 @@ def movimentos(tecla):
         elif tecla == engine.K_DOWN:
             direcao = "y+"
 
+def aumentar():
+    global calda 
+    calda.append(0)
+
+
+def novaComida():
+    global posComida
+    posComida = [randint(tamComida[0], resolucao[0] - tamComida[0]), randint(tamComida[1], resolucao[1] - tamComida[1]), tamComida[0], tamComida[1]]
+
+
+def colisaoComida(obj1, obj2):
+    if obj1.colliderect(obj2):
+        aumentar()
+        novaComida()
+    
+
+def colisaoParede(rect):
+    if rect[0] > resolucao[0] or rect[0] < 1:
+        _exit(0)
+
+    if rect[1] > resolucao[1] or rect[0] < 1:
+        _exit(0)
+
+
 def render():
+    global calda
+    arr = [rectPlayer] * 12
+    calda = [0] * 2
     while(True):
         ## pintar a tela de preto
         tela.fill(0)
 
         ## desenhar a cobra
-        engine.draw.rect(tela, playerCor, rectPlayer)
+        player = engine.draw.rect(tela, playerCor, rectPlayer)
+        arr.append(player)
+
+        for i in range(0, len(calda)):
+            engine.draw.rect(tela, playerCor, arr[len(arr) - 11 * (i +1)])
+
+        comida = engine.draw.rect(tela, playerCor, posComida)
 
         ## fazer a cobra andar um pouco a cada frame
         autoRun()
+        colisaoComida(player, comida)
+        colisaoParede(player)
 
         ## fazer um update senao fica td bugado
         engine.display.update()
@@ -74,14 +111,17 @@ relogio = engine.time.Clock()
 ## nesse caso o tamanho eh 2% do tamanho do display
 tam = [int(resolucao[0] * 0.02)] * 2
 
+tamComida = tam
 ## [0, 1] sao as posiçoes da cobra e [2, 3] eh o tamanho
 rectPlayer = [(resolucao[0] // 2) - tam[0] // 2, (resolucao[1] // 2) - tam[1] // 2, tam[0], tam[1]]
 
 ## calcula a velocidade da cobra com base na resoluçao da tela
 ## vel eh 0,7% da resoluçao
-vel = resolucao[0] * 0.0017
+vel = resolucao[0] * 0.0012
 
 playerCor = 255, 255, 255
 
 ## a variavel q vai definir pra onde a cobra vai se mexer
 direcao = "x+"
+
+novaComida()
